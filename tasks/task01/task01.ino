86d614424ee454bd1b0759f4f07492e43c5820ba
Oleg Moskvitin <116306584+Tvorozh0k@users.noreply.github.com>
2024-02-16 21:00:54 +0400

    Update task01.ino
    Первоначальная версия 1-ой лабораторной работы

--- tasks/task01/task01.ino
+++ tasks/task01/task01.ino
@@ -1,7 +1,42 @@
-void setup() {
+// число ножек
+int num_of_pins = 5;
 
+// ножки 
+int pins[] = {3, 5, 6, 9, 10};
+
+// периодичность миганий
+// unsigned long blinkPeriod[] = {10000, 1000, 500, 100, 50};
+
+unsigned long blinkPeriod[] = {32000000, 16000000, 4000000, 2000000, 1000000};
+
+// текущий момент времени
+unsigned long currentMicros = 0;
+
+// моменты времени, когда происходило последнее мигание
+unsigned long previousBlinkMicros[] = {0, 0, 0, 0, 0};
+
+// состояния лампочек
+int ledState[] = {LOW, LOW, LOW, LOW, LOW};
+
+void setup()
+{
+  // подготовка ножек к работе
+  for (int i = 0; i < num_of_pins; ++i)
+    pinMode(pins[i], OUTPUT);
 }
 
-void loop() {
-
+void loop()
+{
+  currentMicros = micros();
+  
+  for (int i = 0; i < num_of_pins; ++i)
+    if (currentMicros - previousBlinkMicros[i] >= blinkPeriod[i])
+  	{
+    	// обновляем время последнего мигания
+    	previousBlinkMicros[i] = currentMicros;
+    
+    	// обновляем состояние
+    	ledState[i] = (ledState[i] == LOW)? HIGH : LOW;   
+    	digitalWrite(pins[i], ledState[i]);
+  	}
 }
