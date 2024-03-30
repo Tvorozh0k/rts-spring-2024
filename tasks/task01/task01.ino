ee75dc9de26a1924b8ad7ec7e3597f952292ec6a
Oleg Moskvitin <116306584+Tvorozh0k@users.noreply.github.com>
2024-03-30 10:44:20 +0400

    Update task01.ino

--- tasks/task01/task01.ino
+++ tasks/task01/task01.ino
@@ -9,9 +9,7 @@ int num_of_pins = 5;
 int pins[] = {3, 5, 6, 9, 10};
 
 // периодичность миганий
-// unsigned long blinkPeriod[] = {10000, 1000, 500, 100, 50};
-
-unsigned long blinkPeriod[] = {32000000, 16000000, 4000000, 2000000, 1000000};
+unsigned long blinkPeriod[] = {10000, 1000, 500, 100, 50};
 
 // текущий момент времени
 unsigned long currentMicros = 0;
