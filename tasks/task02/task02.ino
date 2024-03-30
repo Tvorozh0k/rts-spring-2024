f6d5d9fee44cf7fab69317b062e187ccaacef97e
Oleg Moskvitin <116306584+Tvorozh0k@users.noreply.github.com>
2024-03-30 10:50:43 +0400

    Update task02.ino

--- tasks/task02/task02.ino
+++ tasks/task02/task02.ino
@@ -2,6 +2,8 @@
 // Группа: 441
 // Задание 2
 
+#include <TaskManagerIO.h>
+
 // число ножек
 int num_of_pins = 5;
 
@@ -9,12 +11,7 @@ int num_of_pins = 5;
 int pins[] = {3, 5, 6, 9, 10};
 
 // периодичность миганий
-// unsigned long blinkPeriod[] = {10000, 1000, 500, 100, 50};
-
-unsigned long blinkPeriod[] = {32000000, 16000000, 4000000, 2000000, 1000000};
-
-// текущий момент времени
-unsigned long currentMicros = 0;
+unsigned long blinkPeriod[] = {10000, 1000, 500, 100, 50};
 
 // моменты времени, когда происходило последнее мигание
 unsigned long previousBlinkMicros[] = {0, 0, 0, 0, 0};
@@ -27,20 +24,64 @@ void setup()
   // подготовка ножек к работе
   for (int i = 0; i < num_of_pins; ++i)
     pinMode(pins[i], OUTPUT);
+
+  taskid_t taskId0 = taskManager.scheduleFixedRate(blinkPeriod[0], [] {
+    if (ledState[0] == LOW) {
+      ledState[0] = HIGH;
+    }
+    else {
+      ledState[0] = LOW;
+    }
+
+    digitalWrite(pins[0], ledState[0]);
+  }, TIME_MICROS);
+
+  taskid_t taskId1 = taskManager.scheduleFixedRate(blinkPeriod[1], [] {
+    if (ledState[1] == LOW) {
+      ledState[1] = HIGH;
+    }
+    else {
+      ledState[1] = LOW;
+    }
+
+    digitalWrite(pins[1], ledState[1]);
+  }, TIME_MICROS);
+
+  taskid_t taskId2 = taskManager.scheduleFixedRate(blinkPeriod[2], [] {
+    if (ledState[2] == LOW) {
+      ledState[2] = HIGH;
+    }
+    else {
+      ledState[2] = LOW;
+    }
+
+    digitalWrite(pins[2], ledState[2]);
+  }, TIME_MICROS);
+
+  taskid_t taskId3 = taskManager.scheduleFixedRate(blinkPeriod[3], [] {
+    if (ledState[3] == LOW) {
+      ledState[3] = HIGH;
+    }
+    else {
+      ledState[3] = LOW;
+    }
+
+    digitalWrite(pins[3], ledState[3]);
+  }, TIME_MICROS);
+
+  taskid_t taskId4 = taskManager.scheduleFixedRate(blinkPeriod[4], [] {
+    if (ledState[4] == LOW) {
+      ledState[4] = HIGH;
+    }
+    else {
+      ledState[4] = LOW;
+    }
+
+    digitalWrite(pins[4], ledState[4]);
+  }, TIME_MICROS);
 }
 
 void loop()
 {
-  currentMicros = micros();
-  
-  for (int i = 0; i < num_of_pins; ++i)
-    if (currentMicros - previousBlinkMicros[i] >= blinkPeriod[i])
-  	{
-    	// обновляем время последнего мигания
-    	previousBlinkMicros[i] = currentMicros;
-    
-    	// обновляем состояние
-    	ledState[i] = (ledState[i] == LOW)? HIGH : LOW;   
-    	digitalWrite(pins[i], ledState[i]);
-  	}
+  taskManager.runLoop();
 }
